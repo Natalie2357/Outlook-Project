52af2583891c7d5ddd0aad1edf07db5c0a427c78
Natalie <pratt.natalie@gmail.com>
2019-05-12 11:22:26 +0300

    send to zahi

--- server/server.js
+++ server/server.js
@@ -1,35 +1,46 @@
 const https = require('https');
 const hostname = '127.0.0.1';
 const port = 443;
-const fs=require('fs');
+const fs = require('fs');
 var certOptions = {
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.crt')
 }
 
-const server = https.createServer(certOptions,(req,res)=>{
+const server = https.createServer(certOptions, (req, res) => {
   const url = req.url.substring(1).split('?')[0];
-  if(url == 'data'){
+  if (url == 'data') {
     const chunks = [];
     req.on('data', chunk => chunks.push(chunk));
     req.on('end', () => {
       var data = Buffer.concat(chunks).toString();
-      try{
+      try {
         data = JSON.parse(data);
         console.log(JSON.stringify(data));
-      }catch(e){}
+      } catch (e) { }
       res.end('thanks')
     })
     return;
   }
-  fs.readFile(url,(data,err)=>{
-    if(err)return res.end(err.toString());
+  if (url.endsWith('html'))
     res.setHeader('Content-Type', 'text/html');
+  else if (url.endsWith('jpg')) {
+    res.setHeader('Content-Type', 'image/jpeg');
+    const read = fs.createReadStream(url)
+    read.pipe(res);
+    return;
+  }
+  else if (url.endsWith('js'))
+    res.setHeader('Content-Type', 'text/javascript');
+  else if (url.endsWith('css'))
+    res.setHeader('Content-Type', 'text/css')
+  fs.readFile(url, (data, err) => {
+    if (err) return res.end(err.toString());
+    if (!data) return res.end('')
     res.end(data.toString());
   });
-  //res.end('hh');
 });
-server.listen(port,hostname,console.log);
+server.listen(port, hostname, console.log);
 // const http = require('http');
 // const hostname = '127.0.0.1';
 // const port = 3000;
